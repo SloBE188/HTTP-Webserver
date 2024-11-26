8e4165d5d8ddd9fb68247fa0131b29da8cf50e13
SloBE188 <nils.burkard@outlook.com>
2024-11-26 17:01:26 +0100

    cleanup

--- server/server.h
+++ server/server.h
@@ -1,5 +1,9 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+char* load_file(const char* filename, long* filesize);
+const char* get_mime_type(const char* path);
+int main(int argc, char** argv);
+
 
 #endif
